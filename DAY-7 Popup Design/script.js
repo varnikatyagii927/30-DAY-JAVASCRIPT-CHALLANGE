aafeb9e47c5714d018796f097bee1c1cce8dd0b3
varnikatyagii927 <81870966+varnikatyagii927@users.noreply.github.com>
2024-08-19 22:34:01 +0530

    Add files via upload

---
+++ DAY-7 Popup Design/script.js
@@ -0,0 +1,8 @@
+let popup = document.getElementById("popup");
+
+function openPopup(){
+    popup.classList.add("open-Popup");
+}
+function closePopup(){
+    popup.classList.remove("open-Popup");
+}
