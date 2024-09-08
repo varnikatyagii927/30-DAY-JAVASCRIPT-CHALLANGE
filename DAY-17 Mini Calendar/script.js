cc9ecbb69b79df2919c97aa731132db02939a32b
varnikatyagii927 <81870966+varnikatyagii927@users.noreply.github.com>
2024-09-08 15:17:19 +0530

    Add files via upload

---
+++ DAY-17 Mini Calendar/script.js
@@ -0,0 +1,14 @@
+const date = document.getElementById("date");
+const day = document.getElementById("day");
+const month = document.getElementById("month");
+const year = document.getElementById("year");
+
+const today = new Date();
+
+const weekDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+const allMonths = ["December", "January", "February", "March", "April", "June", "July", "August", "September", "October", "November"];
+
+date.innerHTML =( today.getDate()<10?"0":"") + today.getDate();
+day.innerHTML = weekDays[today.getDay()];
+month.innerHTML = allMonths[today.getMonth()];
+year.innerHTML = today.getFullYear();
